3ac8f8025e4d9958c9ce954661f19c58e599ba00
JosephRenLei <renyixuan1204@gmail.com>
2020-04-23 12:51:50 +1000

    Updates

---
+++ precache-manifest.222142d508781197d340baec393831c0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c4ab7aedc0aded4827183841ebc8e21",
+    "url": "/react-in-github-demo/index.html"
+  },
+  {
+    "revision": "fbf66b6ab2e74f16a1e8",
+    "url": "/react-in-github-demo/static/css/main.f1a5847a.chunk.css"
+  },
+  {
+    "revision": "0a18d1502b468d1a4b93",
+    "url": "/react-in-github-demo/static/js/2.88c4bf8b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-in-github-demo/static/js/2.88c4bf8b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fbf66b6ab2e74f16a1e8",
+    "url": "/react-in-github-demo/static/js/main.521c1aa9.chunk.js"
+  },
+  {
+    "revision": "d945a44d45886c589ed3",
+    "url": "/react-in-github-demo/static/js/runtime-main.2e494708.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-in-github-demo/static/media/logo.5d5d9eef.svg"
+  }
+]);
